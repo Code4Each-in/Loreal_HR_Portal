012881e441c6b0ef14852a3e3f40cee5ed172710
unknown <lovepreet@code4each.com>
2024-05-20 19:38:46 +0530

    salary head page

--- app/Http/Controllers/SalaryController.php
+++ app/Http/Controllers/SalaryController.php
@@ -1,7 +1,7 @@
 <?php
 
 namespace App\Http\Controllers;
-
+use App\Models\SalaryHead;
 use Illuminate\Http\Request;
 
 class SalaryController extends Controller
@@ -9,5 +9,48 @@ class SalaryController extends Controller
     public function index()
     {
         return view('salary_head');
+    }
+     
+    public function store(Request $req)
+    {
+         ///  dd($req->percentage);
+        // $validated = $req->validate([
+        //     'head_title' => 'required|max:255',
+        //     'formula' => 'required',
+        // ]);
+        $method = $req->method;
+        if($method ==  "wid_formula")
+        {
+            $com =  $req->salary_component ;
+            $symbol =  $req->symbol ;
+            $percentage =  $req->percentage;
+   
+   
+            $formula = "($com $symbol $percentage) / 100";
+            //print_r($formula);
+           // dd($req->percentage);
+           $product = SalaryHead::create([
+               'head_title'        => $req->head_title,
+               'salary_component'  => $req->salary_component,
+               'symbol'            => $req->symbol,
+               'percentage'       => $req->percentage,
+               'formula'           => $formula
+           ]);
+           return redirect()->route('salaryHead')
+           ->with('message', 'Salary Head added successfully!');     
+          }
+        else{
+
+        $product = SalaryHead::create([
+               'head_title'        => $req->head_title,
+               'salary_component'  => "",
+               'symbol'            => "",
+               'amount'            =>$req->amount,
+               'formula'           => ""
+           ]);
+        }
+    
+
+
     }
 }
