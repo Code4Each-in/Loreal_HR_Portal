b8ff38bac48db518ab6dbabc2c81f2811bc6ba6e
unknown <lovepreet@code4each.com>
2024-05-21 13:34:10 +0530

    pushing deleted files

--- storage/framework/views/c2bdbf1ee5ca1cf3e8f8cb33e8296518.php
+++
@@ -1,8 +0,0 @@
-<?php $__env->startSection('title', 'Dashboard'); ?>
-<?php $__env->startSection('sub-title', 'Dashboard'); ?>
-
-
-<?php $__env->startSection('content'); ?>
-lovepreet
-<?php $__env->stopSection(); ?>
-<?php echo $__env->make('layout.app', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH D:\LOreal_HR_Portal\resources\views/Dashboard.blade.php ENDPATH**/ ?>
