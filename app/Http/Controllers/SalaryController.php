fdc24bdc75aee764c2fc0c5c67650dd21d26bd4e
unknown <lovepreet@code4each.com>
2024-05-21 11:56:01 +0530

    remove comments from salary controller

--- app/Http/Controllers/SalaryController.php
+++ app/Http/Controllers/SalaryController.php
@@ -8,49 +8,35 @@ class SalaryController extends Controller
 {
     public function index()
     {
-        return view('salary_head');
+        return view('Salary.salary_head');
     }
      
     public function store(Request $req)
     {
-         ///  dd($req->percentage);
-        // $validated = $req->validate([
-        //     'head_title' => 'required|max:255',
-        //     'formula' => 'required',
-        // ]);
+         
+        $validated = $req->validate([
+            'head_title' => 'required|max:255'
+        ]);
         $method = $req->method;
         if($method ==  "wid_formula")
         {
-            $com =  $req->salary_component ;
-            $symbol =  $req->symbol ;
-            $percentage =  $req->percentage;
-   
-   
-            $formula = "($com $symbol $percentage) / 100";
-            //print_r($formula);
-           // dd($req->percentage);
-           $product = SalaryHead::create([
+           $salary_head = SalaryHead::create([
                'head_title'        => $req->head_title,
-               'salary_component'  => $req->salary_component,
-               'symbol'            => $req->symbol,
-               'percentage'       => $req->percentage,
-               'formula'           => $formula
+               'formula'           => $req->formulaOutput
            ]);
-           return redirect()->route('salaryHead')
-           ->with('message', 'Salary Head added successfully!');     
+      
           }
         else{
 
-        $product = SalaryHead::create([
+           $salary_head = SalaryHead::create([
                'head_title'        => $req->head_title,
-               'salary_component'  => "",
-               'symbol'            => "",
+               'formula'           => '',
                'amount'            =>$req->amount,
-               'formula'           => ""
+         
            ]);
         }
-    
 
-
+        return redirect()->route('salaryHead')
+        ->with('message', 'Salary Head added successfully!');    
     }
 }
